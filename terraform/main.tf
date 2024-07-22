8c07bae3717c8bd26b2d1cec1df4a2e1d8df89a1
satyadarling <60965529+satyadarling@users.noreply.github.com>
2024-07-22 10:58:21 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,68 @@
 provider "aws" {
-  region     = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+    region     = var.region
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0b72821e2f351e396"
-  instance_type = "t2.micro"
+module "ec2_instance" {
+  source = "./ec2-instance"
+
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = var.vpc_security_group_ids
+  tags                   = var.tags
 }
 
-output "instance_ips" {
-  value = aws_instance.example.*.public_ip
+resource "null_resource" "upload_file" {
+  depends_on = [module.ec2_instance]
+
+  provisioner "file" {
+    source      = "~/.ssh/id_rsa.pub"
+    destination = "/home/ubuntu/id_rsa.pub"
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"  # Adjust based on your instance's user
+      private_key = file("/home/jenkins_home/workspace/my_testing/ansible/satya.pem")
+      host        = module.ec2_instance.instance_public_ip
+    }
+  }
+}
+
+resource "null_resource" "move_and_set_permissions" {
+  depends_on = [null_resource.upload_file]
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo mv /home/ubuntu/id_rsa.pub /home/ubuntu/.ssh/id_rsa.pub",
+      "sudo chown ubuntu:ubuntu /home/ubuntu/.ssh/id_rsa.pub",
+      "sudo chmod 600 /home/ubuntu/.ssh/id_rsa.pub"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"  # Adjust based on your instance's user
+      private_key = file("/home/jenkins_home/workspace/my_testing/ansible/satya.pem")
+      host        = module.ec2_instance.instance_public_ip
+    }
+  }
+}
+
+resource "null_resource" "configure_ssh" {
+  depends_on = [null_resource.move_and_set_permissions]
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo sed -i '/^PubkeyAuthentication /d' /etc/ssh/sshd_config",
+      "echo 'PubkeyAuthentication yes' | sudo tee -a /etc/ssh/sshd_config",
+      "sudo systemctl restart ssh"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"  # Adjust based on your instance's user
+      private_key = file("/home/jenkins_home/workspace/my_testing/ansible/satya.pem")
+      host        = module.ec2_instance.instance_public_ip
+    }
+  }
 }
