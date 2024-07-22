6141b6418c1b10fca2cbf687615b0372af8ad3c7
satyadarling <60965529+satyadarling@users.noreply.github.com>
2024-07-22 11:00:19 +0530

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,7 @@
-output "instance_ips" {
-  value = aws_instance.example.*.public_ip
+output "instance_id" {
+  value = module.ec2_instance.instance_id
+}
+
+output "instance_public_ip" {
+  value = module.ec2_instance.instance_public_ip
 }
